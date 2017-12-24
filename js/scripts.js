c48534e87fa5494288fac348e66f272ecdf7d322
AvidEslami <avideslami@gmail.com>
2017-12-24 12:46:02 -0500

    starting

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("hello");
+var stage=1
+
+function start() {
+    //GET IMAGE ELEMENT
+    //Add hidden class
+    var x = document.getElementById("cover"); 
+    x.classList.add("hidden");
+     var z = document.getElementById("board"); 
+    z.classList.remove("hidden");
+    
+}
