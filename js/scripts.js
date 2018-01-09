306565aef8e0f0ab891d18f4ecfd7bb77800894c
AvidEslami <avideslami@gmail.com>
2018-01-08 21:27:12 -0500

    workin

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
 var stage=1
+var score1result=0
+var turn=0
+
 
 function start() {
     //GET IMAGE ELEMENT
     //Add hidden class
     var x = document.getElementById("cover"); 
     x.classList.add("hidden");
-     var z = document.getElementById("board"); 
+    var z = document.getElementById("board"); 
     z.classList.remove("hidden");
     var z = document.getElementById("player1"); 
     z.classList.remove("hidden");
+    var z = document.getElementById("score1");
+    z.classList.remove("hidden");
+    var z = document.getElementById("score1result");
+    z.classList.remove("hidden");
+    var z = document.getElementById("nextbutton");
+    z.classList.remove("hidden");
+}
+
+document.getElementById("score1result").innerText = score1;
+
+function nextround(){
+    turn=turn+1
+    console.log(turn)
+    handlenewturn()
+}
+
+function handlenewturn() {
+      if (turn==1) {
+        var y = document.getElementById("player1");
+        y.classList.add("position1");
+        y.classList.remove("position0");
+        
+    } else if (turn==2) {
+        console.log(turn)
+        
+    }
     
 }
