1689dfbe28bed370b1ea3249c695cba5d21ed1c7
AvidEslami <avideslami@gmail.com>
2018-01-12 21:59:09 -0500

    wheel works

--- js/scripts.js
+++ js/scripts.js
@@ -38,6 +38,58 @@ function handlenewturn() {
         console.log(turn)
         var w = document.getElementById("wheel");
         w.classList.remove("hidden");
+        var s = document.getElementById("wheeltip");
+        s.classList.remove("hidden");
+        setTimeout(function() {
+            
+            var multiplier = 20;
+            var rotateDeg = getRandomInt(360 * multiplier, 720 * multiplier);
+            s.style.transform = "rotate("+ rotateDeg +"deg)";
+            
+            console.log(getSpinnerLocation(rotateDeg, "normalSpinner"));
+            
+        },1000);
+    }else if (turn==3) {
+        
+    }
+    
+    
+}
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getSpinnerLocation(rotateDeg, spinnerType) {
+    
+    var absoluteDeg = rotateDeg % 360;
+    
+    if (spinnerType === "normalSpinner") {
+        
+        if (absoluteDeg < 134 && absoluteDeg >= 0) {
+            //continue
+            return "green";
+        } else if (absoluteDeg < 181 && absoluteDeg >= 134) {
+            //red
+            return "red";
+        } else {
+            //blue section
+            return "blue"
+        }
+        
+    } else {
+        //Other spinner
     }
     
 }
