b2e8a7e0b9dce0d177356d678572b04cc77fc8b0
AvidEslami <avideslami@gmail.com>
2018-01-14 00:36:03 -0500

    q

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ var stage=1
 var score1result=0
 var turn=0
 
-
 function start() {
     //GET IMAGE ELEMENT
     //Add hidden class
@@ -19,8 +18,6 @@ function start() {
     var z = document.getElementById("nextbutton");
     z.classList.remove("hidden");
 }
-
-document.getElementById("score1result").innerText = score1;
 
 function nextround(){
     turn=turn+1
@@ -44,8 +41,8 @@ function handlenewturn() {
         s.classList.remove("hidden");
         setTimeout(function() {
             
-            var multiplier = 1;
-            var rotateDeg = getRandomInt(170 * multiplier, 170 * multiplier);
+            var multiplier = 10;
+            var rotateDeg = getRandomInt(360 * multiplier, 720 * multiplier);
             s.style.transform = "rotate("+ rotateDeg +"deg)";
             
             spinnerValue = getSpinnerLocation(rotateDeg, "normalSpinner");
@@ -79,6 +76,8 @@ function handlenewturn() {
             y.classList.remove("position1");
             var z = document.getElementById("wheeltip");
             z.classList.add("hidden");
+            score1result++;
+            document.getElementById("score1result").innerText = score1result;
         } else if (spinnerValue === "blue") {
             var t = document.getElementById("table");
             t.classList.remove("hidden");
@@ -141,5 +140,105 @@ function getSpinnerLocation(rotateDeg, spinnerType) {
         }
         
     }
+    
+}
+
+function reply_click(innerText) {
+    console.log(innerText);
+    
+    var questionDiv = document.getElementById("question");
+    var questionTextElement = document.getElementById("questionText");
+    var optionAElement = document.getElementById("optionA");
+    var optionBElement = document.getElementById("optionB");
+    var optionCElement = document.getElementById("optionC");
+    var optionDElement = document.getElementById("optionD");
+    
+    questionDiv.dataset.questionSelected = innerText;
+    
+    switch(innerText) {
+        case "1":
+            questionTextElement.innerText = "Question 1?";
+            optionAElement.innerText = "some option a";
+            optionBElement.innerText = "some option b";
+            optionCElement.innerText = "some option c";
+            optionDElement.innerText = "some option d";
+            break;
+        case "2":
+            questionTextElement.innerText = "What color is this?";
+            optionAElement.innerText = "some option a2";
+            optionBElement.innerText = "some option b2";
+            optionCElement.innerText = "some option c2";
+            optionDElement.innerText = "some option d2";
+            break;
+    }
+    
+    questionDiv.classList.remove("hidden");
+
+}
+
+function answer(idClicked) {
+    var questionDiv = document.getElementById("question");
+    var questionId = questionDiv.dataset.questionSelected;
+    
+    if (idClicked === "optionA") {
+        
+        if (questionId === "1" || questionId === "2") {
+            //correct
+            score1result++;
+            document.getElementById("score1result").innerText = score1result;
+            var s = document.getElementById("question");
+            s.classList.add("hidden");
+            var t = document.getElementById("table");
+            t.classList.add("hidden");
+            var p = document.getElementById("player1");
+            p.classList.add("position2");
+            p.classList.remove("position1");
+        } else {
+            //incorrect
+        }
+        
+    } else if (idClicked === "optionB") {
+        if (questionId === "1" || questionId === "2") {
+        score1result++;
+            document.getElementById("score1result").innerText = score1result;
+            var s = document.getElementById("question");
+            s.classList.add("hidden");
+            var t = document.getElementById("table");
+            t.classList.add("hidden");
+            var p = document.getElementById("player1");
+            p.classList.add("position2");
+            p.classList.remove("position1");
+        } else {
+            
+        }
+    } else if (idClicked === "optionC") {
+        if (questionId === "1" || questionId === "2") {
+        score1result++;
+            document.getElementById("score1result").innerText = score1result;
+            var s = document.getElementById("question");
+            s.classList.add("hidden");
+            var t = document.getElementById("table");
+            t.classList.add("hidden");
+            var p = document.getElementById("player1");
+            p.classList.add("position2");
+            p.classList.remove("position1");
+        } else {
+            
+        }
+    } else if (idClicked === "optionD") {
+        if (questionId === "1" || questionId === "2") {
+        score1result++;
+            document.getElementById("score1result").innerText = score1result;
+            var s = document.getElementById("question");
+            s.classList.add("hidden");
+            var t = document.getElementById("table");
+            t.classList.add("hidden");
+            var p = document.getElementById("player1");
+            p.classList.add("position2");
+            p.classList.remove("position1");
+        } else {
+            
+        }
+    } 
     
 }
